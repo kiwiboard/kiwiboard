60aaa128757441d484ef2ae1f2144d100d5ad0d8
kiwiboard <kiwiboard.org@gmail.com>
2013-03-28 13:26:42 +0800

    a demo of servo control
    This program is compatible wit Arduino

---
+++ src/demo_servo/jni/kiwiboard_arduino.cpp
@@ -0,0 +1,41 @@
+#include "kiwiboard.h"
+#include "unistd.h"
+#include "pthread.h"
+
+
+void setup()
+{
+	pinMode(GPIO5, OUTPUT);
+}
+
+// return:
+// 0: stop
+// !0: loop
+int loop()		
+{
+	int i, m;
+	int a1 = 1000;
+	int a2 = 2000;
+	
+	for(i=0; i<30; i++)
+	{
+		for(m=0; m<10; m++)
+		{
+			digitalWrite(GPIO5, HIGH); 
+			delayMicroseconds(a1);
+			digitalWrite(GPIO5, LOW); 
+			delayMicroseconds(20000-a1);
+		}
+		delay(200);
+		for(m=0; m<10; m++)
+		{
+			digitalWrite(GPIO5, HIGH); 
+			delayMicroseconds(a2);
+			digitalWrite(GPIO5, LOW); 
+			delayMicroseconds(20000-a2);
+		}
+		delay(200);		
+	}	
+	
+	return 0;
+}
